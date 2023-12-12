4caeeb132902697e9ad1b07f47ae088535a4d4d8
Baihaqi <baihaqilearning+github@gmail.com>
2023-12-12 11:06:37 +0700

    feat: add create and drop table query

---
+++ app/schema/create.sql
@@ -0,0 +1,103 @@
+CREATE TABLE
+    [dbo].[Book]
+(
+    [BookID]            INT             NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [ISBN]              VARCHAR(13)     NOT NULL,
+    [Title]             VARCHAR(255)    NOT NULL,
+    [Author]            VARCHAR(150)    NOT NULL,
+    [Genre]             VARCHAR(50)     NOT NULL,
+    [PublicationYear]   DATE            NOT NULL,
+    [QuantityAvailable] INT             NOT NULL,
+    [QuantityTotal]     INT             NOT NULL
+);
+
+CREATE TABLE
+    [dbo].[Patron]
+(
+    [PatronId]      INT             NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [FirstName]     VARCHAR(100)    NOT NULL,
+    [LastName]      VARCHAR(100)    NOT NULL,
+    [Email]         VARCHAR(100)    NOT NULL,
+    [PhoneNumber]   VARCHAR(100)    NOT NULL,
+    [Address]       VARCHAR(100)    NOT NULL
+);
+
+CREATE TABLE
+    [dbo].[Loan]
+(
+    [LoanId]        INT     NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [BookId]        INT     NOT NULL,
+    [PatronId]      INT     NOT NULL,
+    [LoanDate]      DATE    NOT NULL DEFAULT GETDATE(),
+    [DueDate]       DATE    NOT NULL,
+    [ReturnDate]    DATE    NULL
+);
+
+CREATE TABLE
+    [dbo].[Fine]
+(
+    [FineId]        INT             NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [PatronId]      INT             NOT NULL,
+    [Amount]        DECIMAL(10,2)   NOT NULL,
+    [PaymentStatus] VARCHAR(10)     NOT NULL,
+    [DueDate]       DATE            NOT NULL
+);
+
+CREATE TABLE
+    [dbo].[Reservation]
+(
+    [ReservationId]     INT     NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [BookId]            INT     NOT NULL,
+    [PatronId]          INT     NOT NULL,
+    [ReservationDate]   DATE    NOT NULL
+);
+
+CREATE TABLE
+    [dbo].[LibraryStaff]
+(
+    [LibraryStaffId]    INT             NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [FirstName]         VARCHAR(100)    NOT NULL,
+    [LastName]          VARCHAR(100)    NOT NULL,
+    [Email]             VARCHAR(100)    NOT NULL,
+    [PhoneNumber]       VARCHAR(100)    NOT NULL
+);
+
+CREATE TABLE
+    [dbo].[User]
+(
+    [UserId]            INT             NOT NULL IDENTITY (1, 1) PRIMARY KEY,
+    [Username]          VARCHAR(100)    NOT NULL,
+    [Password]          VARCHAR(100)    NOT NULL,
+    [Role]              VARCHAR(15)     NOT NULL,
+    [LibraryStaffId]    INT NULL,
+    [PatronId]          INT NULL
+);
+
+
+ALTER TABLE
+    [dbo].[Loan] ADD CONSTRAINT [FK_Loan_Book]
+    FOREIGN KEY ([BookId]) REFERENCES [dbo].[Book] ([BookId]);
+
+ALTER TABLE
+    [dbo].[Loan] ADD CONSTRAINT [FK_Loan_Patron]
+    FOREIGN KEY ([PatronId]) REFERENCES [dbo].[Patron] ([PatronId]);
+
+ALTER TABLE
+    [dbo].[Fine] ADD CONSTRAINT [FK_Fine_Patron]
+    FOREIGN KEY ([PatronId]) REFERENCES [dbo].[Patron] ([PatronId]);
+
+ALTER TABLE
+    [dbo].[Reservation] ADD CONSTRAINT [FK_Reservation_Book]
+    FOREIGN KEY ([BookId]) REFERENCES [dbo].[Book] ([BookId]);
+
+ALTER TABLE
+    [dbo].[Reservation] ADD CONSTRAINT [FK_Reservation_Patron]
+    FOREIGN KEY ([PatronId]) REFERENCES [dbo].[Patron] ([PatronId]);
+
+ALTER TABLE
+    [dbo].[User] ADD CONSTRAINT [FK_User_LibraryStaff]
+    FOREIGN KEY ([LibraryStaffId]) REFERENCES [dbo].[LibraryStaff] ([LibraryStaffId]);
+
+ALTER TABLE
+    [dbo].[User] ADD CONSTRAINT [FK_User_Patron]
+    FOREIGN KEY ([PatronId]) REFERENCES [dbo].[Patron] ([PatronId]);
