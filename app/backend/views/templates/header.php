693423e48634511ef877d6b9b47a6034bbf15af9
Baihaqi <baihaqilearning+github@gmail.com>
2023-12-13 14:14:34 +0700

    feat: add navbar and footer

--- app/backend/views/templates/header.php
+++ app/backend/views/templates/header.php
@@ -7,4 +7,23 @@
     <title>Library - <?= $data['title']; ?></title>
     <link rel="stylesheet" href="<?= BASEURL ?>/css/bootstrap.css">
 </head>
-<body>
+<body style="min-height: 100vh;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?= BASEURL ?>"><img src="<?= BASEURL ?>/img/logo.png" alt="Your Brand Logo" style="max-width: 150px;"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?= BASEURL ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
