96358657132f2b766112b1386566de7983f656e4
Baihaqi <baihaqilearning+github@gmail.com>
2023-12-13 16:44:25 +0700

    feat: connect upload and book list to navbar

--- app/backend/views/templates/header.php
+++ app/backend/views/templates/header.php
@@ -19,10 +19,10 @@
           <a class="nav-link" href="<?= BASEURL ?>">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="<?= BASEURL ?>/upload">Add Book</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="<?= BASEURL ?>/table">Book List</a>
         </li>
       </ul>
     </div>
