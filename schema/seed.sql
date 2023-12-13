e854078053943a71804da9451b92aa7362eb1159
Baihaqi <baihaqilearning+github@gmail.com>
2023-12-13 21:42:35 +0700

    feat: add new query

---
+++ schema/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO [Book] (ISBN, Title, Author, Genre, PublicationYear, QuantityAvailable, QuantityTotal)
+VALUES  ('9783161484100', 'Database 101', 'Ilocos', 'Education', '2023', '27', '28'),
+        ('9783161484101', 'Database 102', 'Ilocos', 'Education', '2023', '27', '28'),
+        ('9783161484102', 'Database 103', 'Ilocos', 'Education', '2023', '27', '28');
+
+INSERT INTO [LibraryStaff] (FirstName, LastName, Email, PhoneNumber)
+VALUES ('Admin', 'Admin', 'admin@local.com', '082243216789');
